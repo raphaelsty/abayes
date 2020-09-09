7c7228f641f3af227f7de4ae6a79229edd35d29a
raphaelsty <raphael.sourty@gmail.com>
2020-09-10 01:37:48 +0200

    Update setup;py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     version=f"{__version__}",
     author="Raphael Sourty",
     author_email="raphael.sourty@gmail.com",
-    description="abayes",
+    description="Autoregressive bayesian linear model",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/raphaelsty/abayes",
