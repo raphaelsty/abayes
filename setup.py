a060f1bc6bafbcdce4e757bd805759c81c0a4707
raphaelsty <raphael.sourty@gmail.com>
2020-09-10 01:37:03 +0200

    Update setup;py

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
     python_requires='>=3.6',
